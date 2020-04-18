640c24f43337caf1d9dfbca1ad479d39f50ec170
uhooi <uhooi@server.fake>
2020-04-18 15:30:55 +0900

    いろいろ変更

---
+++ sample/sample/Modules/QiitaList/Views/QiitaTableViewCell.swift
@@ -0,0 +1,82 @@
+//
+//  QiitaTableViewCell.swift
+//  sample
+//
+//  Created by 水野祥子 on 2020/04/16.
+//  Copyright © 2020 sachiko. All rights reserved.
+//
+
+import UIKit
+
+protocol QiitaTableViewCellProtocol {
+    func didTapReadButton(index: Int)
+}
+
+final class QiitaTableViewCell: UITableViewCell {
+    
+    // MARK: Stored Type Properties
+    
+    static let height: CGFloat = 100.0
+    
+    // MARK: Stored Instance Properties
+    
+    var delegate: QiitaTableViewCellProtocol?
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet private weak var userImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel! {
+        willSet {
+            newValue.textColor = .darkGray
+            newValue.text = nil
+        }
+    }
+    @IBOutlet private weak var readButton: UIButton! {
+        willSet {
+            newValue.setTitleColor(.white, for: .normal)
+        }
+    }
+    
+    // MARK: IBActions
+    
+    @IBAction private func didTapReadButton(_ sender: UIButton) {
+        guard let indexPath = (self.superview as! UITableView).indexPath(for: self) else {
+            return
+        }
+        self.delegate?.didTapReadButton(index: indexPath.row)
+    }
+    
+    // MARK: Other Internal Methods
+    
+    func setup(title: String, isRead: Bool, profileImageURLString: String) {
+        self.titleLabel.text = title
+        // TODO: タイトルと背景色を `setup()` の引数にすれば分岐をなくせるが、UIは手動でテストすると思うので、そこまでしなくていいかもしれない
+        if isRead {
+            self.readButton.setTitle("既読", for: .normal)
+            self.readButton.backgroundColor = .read
+        } else {
+            self.readButton.setTitle("未読", for: .normal)
+            self.readButton.backgroundColor = .unread
+        }
+        self.readButton.isHidden = false
+        
+        // TODO: 画像取得を別クラスで行って分岐をなくす。ライブラリを使ってもいいと思う
+        let imageURL = URL(string: profileImageURLString)!
+        URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
+            if (response as? HTTPURLResponse) != nil {
+                if let data = data {
+                    let image = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        self.userImageView.image = image
+                    }
+                }
+            }
+        }.resume()
+    }
+    
+    func setupError() {
+        self.titleLabel.text = "取得に失敗しました"
+        self.titleLabel.textColor = .darkGray
+        self.readButton.isHidden = true
+    }
+}
