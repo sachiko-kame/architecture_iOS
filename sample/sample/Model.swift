5181341e0ebc14f2fa881ca85a04c4cb9c063b6e
sachiko-kame <sachiko-kame@users.noreply.github.com>
2020-05-04 20:12:16 +0900

    修正前

---
+++ sample/sample/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  sample
+//
+//  Created by 水野祥子 on 2020/05/04.
+//  Copyright © 2020 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+}
---
+++ sample/sample/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  sample
+//
+//  Created by 水野祥子 on 2020/05/04.
+//  Copyright © 2020 sachiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+
+}
